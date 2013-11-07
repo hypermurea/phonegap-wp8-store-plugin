932c848d6f15a4c534319c6d9521eba41a840e21
= <jaakko@hypermurea.com>
2013-11-07 14:18:10 +0200

    methods for review, istrial and open store

--- src/WPStore.cs
+++ src/WPStore.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using Microsoft.Phone.Controls;
 using System.Windows;
 using Microsoft.Phone.Tasks;
+using Microsoft.Phone.Marketplace;
 
 namespace WPCordovaClassLib.Cordova.Commands
 {
@@ -17,9 +18,25 @@ namespace WPCordovaClassLib.Cordova.Commands
     
         public void review(string options)
         {
-
+            (new MarketplaceReviewTask()).Show();
+            DispatchCommandResult();
         }
 
+        public void openStoreToBuyApp(string options) {
+            (new MarketplaceDetailTask()).Show();
+            DispatchCommandResult();
+        }
 
+        public void isTrial(string options) {
+            bool isTrial;
+#if DEBUG
+            isTrial = true; // change this enable debugging in non-trial mode
+#else
+            var licenseInfo = new LicenseInformation();
+            isTrial = licenseInfo.IsTrial();           
+#endif
+            DispatchCommandResult(new PluginResult(PluginResult.Status.OK, "{trial: " + isTrial.ToString().ToLower() + "}"));
+        }
+   
     }
 }
--- www/wpstore.js
+++ www/wpstore.js
@@ -1,7 +1,31 @@
-
     var exec = require('cordova/exec');
 
     var store_export = {};
 
-    module.exports = store_export;
+    store_export.review = function (successCallback, errorCallback) {
 
+        exec(function (res) {
+            successCallback();
+        }, errorCallback, "WPStore", "review", "");
+
+    };
+
+    store_export.isTrial = function (successCallback, errorCallback) {
+
+        exec(function (res) {
+            console.log("" + res);
+            successCallback();
+        }, errorCallback, "WPStore", "isTrial", "");
+
+    };
+
+    store_export.openStoreToBuyApp = function (successCallback, errorCallback) {
+
+        exec(function (res) {
+            var trialdata = JSON.parse(res);
+            successCallback(trialdata);
+        }, errorCallback, "WPStore", "openStoreToBuyApp", "");
+
+    };
+
+    module.exports = store_export;
